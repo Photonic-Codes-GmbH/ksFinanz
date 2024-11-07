8b0d04468d445449c06017ea63951552c05489cf
Christian Wiegand <christianwiegand1987@yahoo.de>
2024-11-07 16:20:35 +0300

    Project Template

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+
+  backendProtocol: 'https',
+  backendDomain: '[NAME].testing.photonic-codes.cloud',
+  backendPort: '',
+  backendPrefix: 'api',
+
+  keycloakUrl: 'https://keycloak.testing.photonic-codes.cloud/auth',
+  keycloakRealm: '',
+  keycloakClientId: '[NAME]-client'
+};
---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+
+  backendProtocol: 'http',
+  backendDomain: 'localhost',
+  backendPort: '3005',
+  backendPrefix: 'api',
+
+  keycloakUrl: 'https://keycloak.testing.photonic-codes.cloud/auth',
+  keycloakRealm: '',
+  keycloakClientId: 'local-[NAME]-client'
+};
